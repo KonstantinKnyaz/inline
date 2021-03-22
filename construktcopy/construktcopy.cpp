20a8014bc9cc8222b83b60497fbdd56da0ba2897
Константин Князев <79729833+KonstantinKnyaz@users.noreply.github.com>
2021-03-22 18:22:59 +0300

    Добавьте файлы проекта.

---
+++ construktcopy/construktcopy.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <clocale>
+#include <string>
+
+using namespace std;
+
+class User {
+private:
+	int age, salary;
+public:
+	User(int age, int salary) {
+		this->age = age;
+		this->salary = salary;
+	}
+
+	inline void printAll();
+
+	User(User &obj) {
+		cout << "Конструктор копирования" << endl;
+		this->age = obj.age;
+		this->salary = obj.salary;
+	}
+};
+
+void User::printAll() {
+		cout << "Возраст: " << this->age << ". Зарплата: " << this->salary << endl;
+	}
+
+inline void hello() {
+	cout << "Hello World" << endl;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	User bob(20, 2000);
+	User jhon(bob);
+	bob.printAll();
+	jhon.printAll();
+
+	hello();
+	
+	return 0;
+}
